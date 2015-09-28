6360fdbf14e6e7ccc08f1b128deaf110b75669b7
Yiming Tsai <ym.owov@gmail.com>
2015-09-27 21:43:42 -0700

    Add standard routing & first controller - p.35

---
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -0,0 +1,5 @@
+class QuotesController < Rulers::Controller
+  def a_quote
+    "String a" + "String b"
+  end
+end
--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,13 +1,28 @@
 require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
   	def call(env)
-  		`echo debug > debug.txt`;
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, {"Content-Type" => "text/html"}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+  		# `echo debug > debug.txt`;
   		[200,{'Content-Type' => 'text/html'},
-  			["Hello!!!!Rulers -- "]] ##{[1, 2, 3].sum}
-
+  			[text]]
   	end
+  end
 
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+    def env
+      @env
+    end
   end
 end
